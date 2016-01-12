9edb79157dfe6fbae841bf305a087d56f6b571d7
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-13 00:46:22 +0800

    Finish section1 - output

---
+++ first_doc.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First PHP DOC</title>
+</head>
+<body>
+    <?php
+        echo("<h1>This is my first php document!</h1>");
+    ?>
+</body>
+</html>
---
+++ output.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <?php
+        echo "This is echoed out!";
+        echo ("<br/>This is also echoed out!");
+        print "<br/>print also works without ()";
+        print("<br/>this is print output!");
+        printf("<br/>I am %d years old.", 26);
+        printf("<br/>The interest rate is %0.2f", 8.73928392);
+    ?>
+</body>
+</html>
